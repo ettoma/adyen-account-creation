53275ee99e1732c2da75e3264641f164b3ffe87c
EttoreToma <ettore.toma@gmail.com>
2022-07-19 23:24:19 +0200

    Client json model

--- go_server/main.go
+++ go_server/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -14,22 +16,65 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	data := url.Values{}
 	data.Set("merchantAccount", "")
 
-	// client := &http.Client{}
+	data1 := map[string]any{
+		"accountHolderCode": "convious_demo",
+		"accountHolderDetails": map[string]any{
+			"address": map[string]any{
+				"country": "asdhsdas", //! <-- invalid country
+			},
+			"businessDetails": map[string]any{
+				"doingBusinessAs":    "Convious",
+				"legalBusinessName":  "Convious BV",
+				"registrationNumber": "123456789",
+				"shareholders": []map[string]any{
+					{
+						"shareholderType": "business",
+						"jobTitle":        "COO",
+						"name": map[string]any{
+							"firstName": "John",
+							"gender":    "MALE",
+							"lastName":  "Kohn",
+						},
+						"address": map[string]any{
+							"country": "NL",
+						},
+						"email": "support@convious.com",
+					},
+				},
+				"email":      "support@convious.com",
+				"webAddress": "www.convious.com",
+			},
+			"legalEntity":    "Business",
+			"processingTier": 1,
+		},
+	}
 
-	r, err := http.NewRequest(http.MethodPost, "https://cal-test.adyen.com/cal/services/Account/v6/createAccountHolder", strings.NewReader((data.Encode())))
+	// convert data1 into url.Values{}
+	data2, _ := json.Marshal(data1)
+	data3 := strings.NewReader(string(data2))
+
+	client := &http.Client{}
+
+	r, err := http.NewRequest(http.MethodPost, "https://cal-test.adyen.com/cal/services/Account/v6/createAccountHolder", data3)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r.Header.Add("Content-Type", "application/json")
-	r.Header.Add("Access-Control-Allow-Origin", "*")
+	// r.Header.Add("Content-Type", "application/json")
+	// r.Header.Add("Access-Control-Allow-Origin", "*")
 	r.Header.Add("x-API-Key", "")
 
-	resp, _ := http.DefaultClient.Do(r)
+	resp, err := client.Do(r)
 
-	fmt.Println(r.Header)
-	fmt.Println(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, _ := io.ReadAll(resp.Body)
+
+	fmt.Println(resp.Status)
+	fmt.Println(string(body))
 
 }
 
